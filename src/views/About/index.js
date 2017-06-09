ca774f4864245fe0bbfdfa8a816f59cf3b050950
Mdjurdj1 <mdjurdj1@gmail.com>
2017-06-08 23:17:10 -0400

    image and styling

--- src/views/About/index.js
+++ src/views/About/index.js
@@ -20,14 +20,15 @@ export default class About extends Component {
           </header>
         <div id="about_content">
           <p>Hey there! I'm Mike.</p>
-          <p> I'm a Full-Stack Web Developer based in New York. I
+          <p className="blue"> I'm a Full-Stack Web Developer based in New York. I
             work with a broad range of languages and frameworks - including React, Ruby, Rails, Node, Express, Ember and several more. I love creating
             simple, responsive websites for the world to use.
           </p>
-          <p> <img id="my_pic" src='../../itsame.png' /> I'm a former English major turned Law School Student turned Entrepeneur turned Developer. What does that
-            mean for you? Well, I've got the kind of diverse background and focused work ethic you want for your team.
+          <p> <img id="my_pic" src='../../itsame.png' />I'm a former English major turned Law School Student turned Entrepeneur turned Developer. What does that
+            mean for you? Well, I've got the kind of diverse background you want for your team, and I bring the work ethic you need for your project ideas.
           </p>
-          <p>Want to reach out and work together? Contact me here.</p>
+          <p>I'm also a big fan of sci-fi thrillers and dystopian fiction.
+           I'm re-reading the Dune series right now, which is one of my all-time favorites.</p>
         </div>
         </div>
        </CSSTransitionGroup>
