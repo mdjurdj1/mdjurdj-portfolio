0b6a2dfed3fbe64c45d2659f96cad8c30166f545
Mdjurdj1 <mdjurdj1@gmail.com>
2017-06-10 18:07:40 -0400

    Started fixing work page

--- src/components/WorkImages/index.js
+++ src/components/WorkImages/index.js
@@ -23,20 +23,27 @@ export default class WorkImage extends Component {
   render() {
     if(this.state.hover) {
       return (
-        <div className="workImage hover" onMouseLeave={this.handleLeave}>
+        <div className="workImage hover">
           <Image
+            onMouseEnter={this.handleFocus}
+            onMouseLeave={this.handleLeave}
+            fluid
+            label={{ as: 'string', color: 'blue', content: this.props.name, icon: this.props.icon, ribbon: true }}
              src={this.props.src}
              as='a'
              href={this.props.link}
              target='_blank'
            />
-          <p>{this.props.name}</p>
         </div>
       )
     } else {
       return (
-        <div className="workImage" onMouseEnter={this.handleFocus}>
+        <div className="workImage">
         <Image
+          onMouseEnter={this.handleFocus}
+          onMouseLeave={this.handleLeave}
+           fluid
+           label={{ as: 'string', color: 'blue', content: this.props.name, icon: this.props.icon, ribbon: true }}
            src={this.props.src}
            as='a'
            href={this.props.link}
--- src/containers/WorkImagesContainer/index.js
+++ src/containers/WorkImagesContainer/index.js
@@ -9,28 +9,28 @@ export default class WorkImagesContainer extends Component {
         <Grid >
          <Grid.Row columns={2}>
            <Grid.Column>
-             <WorkImage name={"Minitube"} language={"React"} src={"../miniTube.jpg"} link={"https://mini-tube-client.herokuapp.com"}/>
+             <WorkImage name={"Minitube"} icon={"desktop"} language={"React"} src={"../miniTube.jpg"} link={"https://mini-tube-client.herokuapp.com"}/>
            </Grid.Column>
            <Grid.Column>
-              <WorkImage name={"Appointable"} language={"Rails"} src={"../appointable.png"} link={'https://www.youtube.com/watch?v=Y73TIgQ_AR8'}/>
+              <WorkImage name={"Appointable"} icon={"youtube play"} language={"Rails"} src={"../appointable.png"} link={'https://www.youtube.com/watch?v=Y73TIgQ_AR8'}/>
            </Grid.Column>
         </Grid.Row>
 
         <Grid.Row columns={2}>
             <Grid.Column>
-               <WorkImage name={"DJ's Computer Repairs"} language={"React"} src={"../djcr.jpg"} link={"http://DJsComputerRepair.com"} />
+               <WorkImage name={"DJ's Computer Repairs"} icon={"desktop"} language={"React"} src={"../djcr.jpg"} link={"http://DJsComputerRepair.com"} />
             </Grid.Column>
             <Grid.Column>
-              <WorkImage name={"Gains-Tracker"} src={"../gainstracker.png"} language={"Sinatra"} link={"https://www.youtube.com/watch?v=_NUHQjUVIEM&feature=youtu.be"} />
+              <WorkImage name={"Gains-Tracker"} icon={"youtube play"} src={"../gainstracker.png"} language={"Sinatra"} link={"https://www.youtube.com/watch?v=_NUHQjUVIEM&feature=youtu.be"} />
             </Grid.Column>
         </Grid.Row>
 
         <Grid.Row columns={2}>
           <Grid.Column>
-             <WorkImage name={"Gameday CLI Gem"} language={"Ruby"} src={"../gameday.jpg"} link={"https://www.youtube.com/watch?v=EJW2hNurpHk"} />
+             <WorkImage name={"Gameday CLI Gem"} icon={"youtube play"} language={"Ruby"} src={"../gameday.jpg"} link={"https://www.youtube.com/watch?v=EJW2hNurpHk"} />
           </Grid.Column>
           <Grid.Column>
-             <WorkImage name={"Code && Coffee"} language={"Blog"} src={"../blog.jpg"} link={"http://mdjurdj.com"} />
+             <WorkImage name={"Code && Coffee"} icon={"desktop"} language={"Blog"} src={"../blog.jpg"} link={"http://mdjurdj.com"} />
           </Grid.Column>
        </Grid.Row>
       </Grid>
