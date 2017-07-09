8e08595a5cc6f527b97d9ca7f90dccb1bdb149f4
Mdjurdj1 <mdjurdj1@gmail.com>
2017-07-09 14:07:25 -0400

    Added Angular project to work page

--- src/containers/WorkImagesContainer/index.js
+++ src/containers/WorkImagesContainer/index.js
@@ -10,14 +10,18 @@ export default class WorkImagesContainer extends Component {
         <div className="work_row">
           <WorkImage name={"Minitube"} icon={"desktop"} language={"React"} src={"../miniTube.jpg"} link={"https://mini-tube-client.herokuapp.com"}/>
           <WorkImage name={"Appointable"} icon={"youtube play"} language={"Rails"} src={"../appointable.png"} link={'https://www.youtube.com/watch?v=Y73TIgQ_AR8'}/>
-          <WorkImage name={"DJ's Computer Repairs"} icon={"desktop"} language={"React"} src={"../djcr.jpg"} link={"http://DJsComputerRepair.com"} />
+          <WorkImage name={"DJ's Kitchen & Bar"} icon={"desktop"} language={"Angular"} src={"../ng_rest.jpg"} link={"http://Dj-kitchen.herokuapp.com"} />
         </div>
 
         <div className="work_row">
           <WorkImage name={"Gains-Tracker"} icon={"youtube play"} src={"../gainstracker.png"} language={"Sinatra"} link={"https://www.youtube.com/watch?v=_NUHQjUVIEM&feature=youtu.be"} />
           <WorkImage name={"Gameday CLI Gem"} icon={"youtube play"} language={"Ruby"} src={"../gameday.jpg"} link={"https://www.youtube.com/watch?v=EJW2hNurpHk"} />
-          <WorkImage name={"Code && Coffee"} icon={"desktop"} language={"Blog"} src={"../blog.jpg"} link={"http://mdjurdj.com"} />
+          <WorkImage name={"DJ's Computer Repairs"} icon={"desktop"} language={"React"} src={"../djcr.jpg"} link={"http://DJsComputerRepair.com"} />
        </div>
+
+       <div className="work_row">
+         <WorkImage name={"Code && Coffee"} icon={"desktop"} language={"Blog"} src={"../blog.jpg"} link={"http://mdjurdj.com"} />
+      </div>
      </div>
     )
   }
