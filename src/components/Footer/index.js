afd195643ad94ad82dc035f44e9ce9535a2fa98e
Mdjurdj1 <mdjurdj1@gmail.com>
2017-07-23 23:20:41 -0400

    Added Footer component skeleton

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import {Icon} from 'semantic-ui-react'
+import {Link} from 'react-router-dom'
+import './footer.css'
+
+export default class Footer extends Component {
+  render() {
+      return (
+        <footer id="footer">
+          
+        </footer>
+      )
+    }
+}
