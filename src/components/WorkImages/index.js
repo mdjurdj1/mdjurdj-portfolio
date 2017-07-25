983d1eaeda5335e224386c40792030226cc3bda2
Mdjurdj1 <mdjurdj1@gmail.com>
2017-07-24 20:03:20 -0400

    changed projects on portfolio to contain descriptions

--- src/components/WorkImages/index.js
+++ src/components/WorkImages/index.js
@@ -1,57 +1,27 @@
 import React, {Component} from 'react'
-import { Image } from 'semantic-ui-react'
+import { Image, Icon } from 'semantic-ui-react'
 import "./workImage.css"
 
 export default class WorkImage extends Component {
-  state = {
-    hover: false
-  }
-
-  handleFocus = () => {
-    this.setState({
-      hover: true
-    })
-  }
-
-  handleLeave = () => {
-    this.setState({
-      hover: false
-    })
-  }
 
   render() {
-    if(this.state.hover) {
       return (
-        <div className="workImage hover">
+        <div className="workImage">
           <Image
             onMouseEnter={this.handleFocus}
             onMouseLeave={this.handleLeave}
             fluid
-            label={{ as: 'string', color: 'blue', content: this.props.name, icon: this.props.icon, ribbon: true }}
              src={this.props.src}
              as='a'
              href={this.props.link}
              target='_blank'
            />
-            <p className="language">{this.props.language}</p>
+           <div className="project_info">
+            <p className="project_name">{this.props.name}</p>
+            <p className="project_language">{this.props.language}</p>
+            <p className="project_description">{this.props.description}</p>
+           </div>
         </div>
       )
-    } else {
-      return (
-        <div className="workImage">
-          <Image className="work_image"
-            onMouseEnter={this.handleFocus}
-            onMouseLeave={this.handleLeave}
-             fluid
-             label={{ as: 'string', color: 'black', content: this.props.name, icon: this.props.icon, ribbon: true }}
-             src={this.props.src}
-             as='a'
-             href={this.props.link}
-             target='_blank'
-           />
-          <p className="language">{this.props.language}</p>
-        </div>
-      )
-    }
   }
 }
--- src/containers/WorkImagesContainer/index.js
+++ src/containers/WorkImagesContainer/index.js
@@ -3,24 +3,34 @@ import WorkImage from '../../components/WorkImages'
 import { Grid } from 'semantic-ui-react'
 import "./workImagesContainer.css"
 
+const project_descriptions = [
+  "A sleek, react-redux based. minimalist youtube browser that persists user data and playlists via the youtube API.",
+  "An appointment managing CMS that tracks user appointments, contacts, and business locations in a secure database.",
+  "An Angular 1.5.11 based single-page application with a fully-featured gulpfile and google maps API integration.",
+  "A simple workout tracker built on the streamlined Sinatra framework.",
+  "A published Ruby gem which utilizes Nokogiri to scrape sports webpages for upcoming events.",
+  "A react-based single-page application for a small, family owned computer repair business.",
+  "My personal blog, speaking on my journey as a professional developer and the languages I love most."
+]
+
 export default class WorkImagesContainer extends Component {
   render() {
     return (
       <div>
         <div className="work_row">
-          <WorkImage name={"Minitube"} icon={"desktop"} language={"React · Redux"} src={"../miniTube.jpg"} link={"https://mini-tube-client.herokuapp.com"}/>
-          <WorkImage name={"Appointable"} icon={"youtube play"} language={"Rails · Jquery"} src={"../appointable.png"} link={'https://www.youtube.com/watch?v=Y73TIgQ_AR8'}/>
-          <WorkImage name={"DJ's Kitchen & Bar"} icon={"desktop"} language={"Angular"} src={"../ng_rest.jpg"} link={"http://Dj-kitchen.herokuapp.com"} />
+          <WorkImage name={"Minitube"} icon={"desktop"} language={"React · Redux"} src={"../miniTube.jpg"} description={project_descriptions[0]} link={"https://mini-tube-client.herokuapp.com"}/>
+          <WorkImage name={"Appointable"} icon={"youtube play"} language={"Rails · Jquery"} src={"../appointable.png"} description={project_descriptions[1]} link={'https://www.youtube.com/watch?v=Y73TIgQ_AR8'}/>
+          <WorkImage name={"DJ's Kitchen & Bar"} icon={"desktop"} language={"Angular"} src={"../ng_rest.jpg"} description={project_descriptions[2]} link={"http://Dj-kitchen.herokuapp.com"} />
         </div>
 
         <div className="work_row">
-          <WorkImage name={"Gains-Tracker"} icon={"youtube play"} src={"../gainstracker.png"} language={"Sinatra"} link={"https://www.youtube.com/watch?v=_NUHQjUVIEM&feature=youtu.be"} />
-          <WorkImage name={"Gameday CLI Gem"} icon={"youtube play"} language={"Ruby"} src={"../gameday.jpg"} link={"https://www.youtube.com/watch?v=EJW2hNurpHk"} />
-          <WorkImage name={"DJ's Computer Repairs"} icon={"desktop"} language={"React"} src={"../djcr.jpg"} link={"http://DJsComputerRepair.com"} />
+          <WorkImage name={"Gains-Tracker"} icon={"youtube play"} src={"../gainstracker.png"} language={"Sinatra"} description={project_descriptions[3]} link={"https://www.youtube.com/watch?v=_NUHQjUVIEM&feature=youtu.be"} />
+          <WorkImage name={"Gameday CLI Gem"} icon={"youtube play"} language={"Ruby"} src={"../gameday.jpg"} description={project_descriptions[4]} link={"https://www.youtube.com/watch?v=EJW2hNurpHk"} />
+          <WorkImage name={"DJ's Computer Repairs"} icon={"desktop"} language={"React"} src={"../djcr.jpg"} description={project_descriptions[5]} link={"http://DJsComputerRepair.com"} />
        </div>
 
        <div className="work_row">
-         <WorkImage name={"Code && Coffee"} icon={"desktop"} language={"My Blog"} src={"../blog.jpg"} link={"http://mdjurdj.com"} />
+         <WorkImage name={"Code && Coffee"} icon={"desktop"} language={"My Blog"} src={"../blog.jpg"} description={project_descriptions[6]} link={"http://mdjurdj.com"} />
       </div>
      </div>
     )
